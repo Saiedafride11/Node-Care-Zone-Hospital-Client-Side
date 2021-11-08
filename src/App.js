be06a21aef1d757d635e19d87c22adac8932cc90
Dark coders <mdsaied.afride@gmail.com>
2021-11-07 20:04:32 -0800

    added basic login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home/Home/Home';
 import Appointment from './pages/Appointment/Appointment/Appointment';
+import Login from './pages/Login/Login/Login';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
               </Route>
               <Route path="/appointment">
                   <Appointment></Appointment>
+              </Route>
+              <Route path="/login">
+                  <Login></Login>
               </Route>
           </Switch>
       </Router>
---
+++ src/pages/Login/Login/Login.js
@@ -0,0 +1,54 @@
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import login from '../../../images/login.png';
+
+const Login = () => {
+    const [loginData, setLoginData] = useState({});
+
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+    const handleLoginSubmit = e => {
+        e.preventDefault();
+    }
+    return (
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item sx={{ mt: 8 }} xs={12} md={6}>
+                    <Typography variant="body1" gutterBottom>Login</Typography>
+                    <form onSubmit={handleLoginSubmit}>
+                         <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Email"
+                            name="email"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                         <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Password"
+                            type="password"
+                            name="password"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                            <Button sx={{ width: '75%', m: 1 }} type="submit" variant="contained">Login</Button>
+                    </form>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{width: '100%'}} src={login} alt="" />
+                </Grid>
+            </Grid>
+            <NavLink to="/home">
+                <Button color="inherit">Back Home</Button>
+            </NavLink>
+        </Container>
+    );
+};
+
+export default Login;
